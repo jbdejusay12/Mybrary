1ee08630312918718bfb6f310c9abb54b3aef036
JHONAS DEJUSAY <jdejusay@gotabrand.com>
2022-11-23 09:53:23 +0800

    0.2

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Mybrary
 
+
+### 0.2
+- fix db connection
 ### 0.1
 - init
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
-
+useNewUrlParser:true
 })
 
 const db = mongoose.connection
